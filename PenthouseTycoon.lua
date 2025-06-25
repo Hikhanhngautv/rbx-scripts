979ad7b1273c97049fbbf31de4c93856edd3d3dd
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-06-25 10:37:43 +0700

    Update PenthouseTycoon.lua

--- PenthouseTycoon.lua
+++ PenthouseTycoon.lua
@@ -1,6 +1,5 @@
-while true do
-    wait(1)
-    for _,v in pairs(game:GetDescendants()) do
+while task.wait(0.5) do
+    for _, v in ipairs(game:GetDescendants()) do
         if v:IsA("ProximityPrompt") and v.ActionText == "Collect"
 then
             fireproximityprompt(v)
