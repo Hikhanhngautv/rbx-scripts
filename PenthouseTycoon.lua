760ddbfaa4ccc5e330b19d62bcd9c499f5ecb1fa
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-06-25 10:57:16 +0700

    Update PenthouseTycoon.lua

--- PenthouseTycoon.lua
+++ PenthouseTycoon.lua
@@ -1,11 +1,11 @@
-while true do
-    task.wait(1)
-    for _, v in pairs(game:GetDescendants()) do
-        if v:IsA("ProximityPrompt") and v.ActionText == "Collect"
-then
-            pcall(function()
-                fireproximityprompt(v)
-            end)
+task.spawn(function()
+    while task.wait(1) do
+        for _, v in pairs(game:GetDescendants()) do
+            if v:IsA("ProximityPrompt") and v.ActionText == "Collect" then
+                pcall(function()
+                    fireproximityprompt(v)
+                end)
+            end
         end
     end
-end
+end)
