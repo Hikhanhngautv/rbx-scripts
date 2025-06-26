d575505c5a1ddb14a87eb70c9a8bc4d4bbce167e
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-06-26 21:00:14 +0700

    Update PenthouseTycoon.lua

--- PenthouseTycoon.lua
+++ PenthouseTycoon.lua
@@ -21,31 +21,44 @@ local AutoFarm = Window:CreateTab("🏠 Auto Farm", 4483362458)
 local AutoCollect = false
 
 AutoFarm:CreateToggle({
-Name = "💸 Auto Collect Cash",
-CurrentValue = false,
-Flag = "AutoCashCollect",
-Callback = function(Value)
-AutoCollect = Value
+	Name = "💸 Auto Collect Cash",
+	CurrentValue = false,
+	Flag = "AutoCashCollect",
+	Callback = function(Value)
+		AutoCollect = Value
 
-if AutoCollect then  
-		task.spawn(function()  
-			while AutoCollect do  
-				task.wait(1)  
+		if AutoCollect then
+			task.spawn(function()
+				while AutoCollect do
+					task.wait(1)
 
-				for _, v in pairs(workspace:GetDescendants()) do  
-					-- Tìm ProximityPrompt gắn với Mailbox  
-					if v:IsA("ProximityPrompt") and v.Parent and tostring(v.Parent):lower():find("mailbox") then  
-						fireproximityprompt(v)  
+					local playerTycoon = nil
+					-- Tìm tycoon của người chơi
+					for _, tycoon in pairs(workspace.Tycoons:GetChildren()) do
+						if tycoon:FindFirstChild("Owner") and tycoon.Owner.Value == game.Players.LocalPlayer then
+							playerTycoon = tycoon
+							break
+						end
+					end
 
-					-- Tìm ClickDetector gắn với Mailbox  
-					elseif v:IsA("ClickDetector") and v.Parent and tostring(v.Parent):lower():find("mailbox") then  
-						fireclickdetector(v)  
-					end  
-				end  
-			end  
-		end)  
-	end  
-end,
+					if playerTycoon then
+						local mailbox = playerTycoon:FindFirstChild("Mailbox", true)
+						if mailbox then
+							-- Ưu tiên ClickDetector
+							local click = mailbox:FindFirstChildOfClass("ClickDetector")
+							if click then
+								fireclickdetector(click)
+							end
 
+							-- Hoặc dùng ProximityPrompt nếu không có ClickDetector
+							local prompt = mailbox:FindFirstChildOfClass("ProximityPrompt")
+							if prompt then
+								fireproximityprompt(prompt)
+							end
+						end
+					end
+				end
+			end)
+		end
+	end,
 })
-
