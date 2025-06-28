7a330631b59a53c1b5b24b47df290547eeff5d0c
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-06-28 09:24:09 +0700

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,14 @@
+--[[
+
+	Rayfield Interface Suite
+	by Sirius
+
+	shlex | Designing + Programming
+	iRay  | Programming
+	Max   | Programming
+
+]]
+
+if debugX then
+	warn('Initialising Rayfield')
+end
